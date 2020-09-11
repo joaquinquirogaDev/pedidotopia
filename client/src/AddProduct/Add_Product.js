3b9b537e5a2c2fb91bcb07c1b371fc9d676da89c
joaquinquiroga22 <joaquinquiroga22@gmail.com>
2020-09-11 11:29:39 -0300

    dhol

--- client/src/AddProduct/Add_Product.js
+++ client/src/AddProduct/Add_Product.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import styles from "./Add_Product.module.css"
 
 //Material-ui
@@ -15,50 +15,82 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(0.7),
     },
   }));
+   const [input, setInput] = useState({
+     Product: "",
+     Proveedor: "",
+     Price: 0,
+     Stock: 0,
+   });
 
-
-
-
-export default function Add_Product (){
+   const onSubmitHandle = function (event) {
+     event.preventDefault();
+     const data = {
+       Product: input.Product,
+       Price: input.Price,
+       Proveedor: input.Proveedor,
+       Stock: input.Stock,
+     };
+   }
+   const handleInputChange = function (e) {
+     setInput({
+       ...input,
+       [e.target.name]: e.target.value,
+     });
+     if (e.target.name === "Price" || e.target.name === "Stock") {
+       setInput({ ...input, [e.target.name]: Number(e.target.value) });
+    }
+   };
+  export default function Add_Product (){
+    const [renderUpdate, setRenderUpdate] = useState(false);
     const classes = useStyles();
     return(
-        <form className = {styles.form}>
+        <form className = {styles.form} onChange = {onSubmitHandle}>
             <div className = {styles.contenedor}>
                 
             <div className={styles.inputcontenedor}>
             <i className={styles.icon}>{<LabelIcon />}</i>    
             <input 
+                value = {input.Product && input.name}
                 type="text"
                 placeholder="Producto"
+                onChange = {handleInputChange}
               />
             </div>
             <div className={styles.inputcontenedor}>
             <i className={styles.icon}>{<DescriptionIcon />}</i>    
             <input className = {styles.input}
+                value = {input.Proveedor}
                 type="text"
                 placeholder="Prooveedor"
+                onChange = {handleInputChange}
               />
             </div>
             <div className={styles.inputcontenedor}>
             <i className={styles.icon}>{<AttachMoneyIcon/>}</i>    
             <input className = {styles.input}
+                 value = {input.Price}
                 type="number"
                 placeholder="Precio"
+                onChange = {handleInputChange}
               />
             </div>
             <div className={styles.inputcontenedor}>
             <i className={styles.icon}>{<ShoppingBasketOutlinedIcon/>}</i>    
             <input className = {styles.input}
+                // value = {input.Stock}
                 type="number"
                 placeholder="Stock"
+                 onChange = {handleInputChange}
               />
             </div>
             <div className = {styles.buttons}>
             <Button
+                onClick = {setRenderUpdate}
                 variant="contained"
                 color="primary"
                 className={classes.button}
                 endIcon={<InputIcon />}
+                
               >
                 Agregar
               </Button>
@@ -68,12 +100,12 @@ export default function Add_Product (){
                   className={classes.button}
                   endIcon={<CancelPresentationRoundedIcon />}
                   href = "/table"
-
+                  
                 >
                 Cancelar
                 </Button>
             </div>
             </div>
         </form>
-    )
+    );
 }   
